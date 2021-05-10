9874f5c980dd580b98063ccc909f560c3643a4c7
biderya <51729071+biderya@users.noreply.github.com>
2021-05-10 23:17:54 +0800

    index.php title oorchlov

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['fname']) && isset($_SESSION['fname'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css" />
-    <title>lab-1 IT301 </title>
+    <title>Лаб</title>
 </head>
 
 <body>
